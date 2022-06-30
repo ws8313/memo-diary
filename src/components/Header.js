4ba0ca978fa06a470135d24b5622864d4c35c3ab
ws8313 <ws8313@naver.com>
2022-07-01 00:11:04 +0900

    feat: install react-router-dom, header css, main css

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,56 @@ const Wrapper = styled.div`
   text-align: center;
 
   width: 100%;
-  height: 50px;
+  height: 4rem;
 
-  border: 1px solid black;
+  /* background-color: #424245;
+  color: white; */
+  border-bottom: 1px solid black;
 
-  .year_month {
+  .header {
+    width: 100%;
     display: flex;
-    justify-content: space-evenly;
     align-items: center;
-    
-    width: 6rem;
-    height: 100%;
-    font-size: 1.5rem;
-    font-weight: 600;
+    justify-content: center;
+
+    .home {
+      width: 3rem;
+      height: 100%;
+      font-size: 1.6rem;
+      font-weight: bold;
+    }
+
+    .title {
+      width: 14rem;
+      height: 100%;
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
   }
 `;
 
 const Header = () => {
-  const today = {
-    year: new Date().getFullYear(), // 오늘의 연도
-    month: new Date().getMonth() + 1, // 오늘의 월, + 1 하는 이유는 월이 0 부터 시작하기 때문
-    date: new Date().getDate(), // 오늘의 날짜
-    day: new Date().getDay(), // 오늘의 요일
-  };
+  // const today = {
+  //   year: new Date().getFullYear(), // 오늘의 연도
+  //   month: new Date().getMonth() + 1, // 오늘의 월, + 1 하는 이유는 월이 0 부터 시작하기 때문
+  //   date: new Date().getDate(), // 오늘의 날짜
+  //   day: new Date().getDay(), // 오늘의 요일
+  // };
 
-  console.log(today);
+  // console.log(today);
 
   return (
     <Wrapper>
-      <div></div>
-      <div className="year_month">
-        <p>{today.year}년</p>
-        <p>{today.month}월</p>
-      </div>
+      <header className="header">
+        {/* <nav className="home">
+          <button>메뉴</button>
+        </nav> */}
+        <div className="title">
+          {/* <p>{today.year}년</p>
+          <p>{today.month}월</p> */}
+          <h1>메모 {`&`} 다이어리</h1>
+        </div>
+      </header>
     </Wrapper>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css'
 import styled from "styled-components";
 
 const Main = () => {
@@ -17,8 +18,49 @@ const Main = () => {
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 400px;
-  border: 1px solid rgba(128, 128, 128, 0.267);
+  height: 100%;
+  /* border: 1px solid black; */
+  border-top: none;
+
+  .react-calendar {
+  width: 100%;
+  background: white;
+  /* border: 1px solid black; */
+  border: none;
+  font-family: 'Nanum Pen Script', Arial, Helvetica, sans-serif;
+  line-height: 1.125em;
+  }
+
+  .react-calendar__navigation {
+    height: 4rem;
+
+    .react-calendar__navigation__label {
+      font-family: 'Nanum Pen Script', Arial, Helvetica, sans-serif;
+      font-size: 1.4rem;
+      font-weight: bold;
+    }
+
+    .react-calendar__navigation__arrow {
+      font-size: 1.4rem;
+      font-weight: bold;
+    }
+  }
+
+  .react-calendar__month-view__weekdays {
+    height: 4rem;
+    font-size: 1.6rem;
+    align-items: center;
+
+    & abbr {
+      text-decoration: none;
+    }
+  }
+  .react-calendar__tile {
+    font-family: 'Nanum Pen Script', Arial, Helvetica, sans-serif;
+    height: 4rem;
+    font-size: 1.4rem;
+  }
+
   
 `;
 
