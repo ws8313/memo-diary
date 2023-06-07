184ac9da7c029239d2fd601b18d78bcf124c4c02
Wooseong Kim <kimwooseong8313@gmail.com>
2023-06-07 22:30:52 +0900

    chore: firebase 수정 및 테스트

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,32 @@
+import { getDoc, doc } from 'firebase/firestore/lite';
+import { db } from '../../../firebase';
+
 const HomePage = () => {
-  return <div>메인 화면</div>;
+  const nickname = 'kDZefLkcRiPOZoCaDRII';
+
+  const getDiaries = async (nickname: string) => {
+    const diaries = doc(db, 'diaries', nickname);
+    const diariesSnapshot = await getDoc(diaries);
+    console.log(1);
+
+    if (!diariesSnapshot.exists()) {
+      return null;
+    }
+    console.log(diariesSnapshot.data());
+
+    return diariesSnapshot.data();
+  };
+
+  // const addComment = (nickname, comment) => {
+  //   const commentRef = doc(db, 'diaries', nickname);
+  //   setDoc(commentRef, comment, { merge: true });
+  // };
+
+  return (
+    <div>
+      <button onClick={() => getDiaries(nickname)}>조회</button>
+    </div>
+  );
 };
 
 export default HomePage;
